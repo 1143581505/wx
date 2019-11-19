ee3050563fec62cfd966d523ff974d94dd8ef018
lancela <1690319247@qq.com>
2019-11-19 13:34:49 +0800

    zl

--- wxworksapce/pages/userdurg/userdurg.js
+++ wxworksapce/pages/userdurg/userdurg.js
@@ -1,100 +1,104 @@
+// pages/userdurg/userdurg.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-	buttonList:[{
-		name:"用药反馈",
-		url:"",
-	},{
-		name:"实时病例",
-		url:"",
-	},{
-		name:"医生建议",
-		url:"",
-	},{
-		name:"联系我们",
-		url:"",	
-	}],
-	illness:true,
-	userdurg:true,
-
+	docName:'',
+	showSign:true
   },
-	click(){
+	signOk(e){
 		this.setData({
-			illness:false
-		})
-	},
-	click2(){
-		this.setData({
-			userdurg:false
-		})
-	},
-	submit(){
-		wx.showToast({
-		  title: '谢谢你的反馈',
-		  icon: 'success',
-		  duration: 2000
+			docName:e.detail.value
 		});
-		this.setData({
-			userdurg:true,
-			illness:true
+		var that =this;
+		wx.showModal({
+		  title: '提示',
+		  content: '请确认你的签名',
+		  success (res) {
+		    if (res.confirm) {
+		      
+			  that.setData({
+				  showSign:false
+			  })
+		    } else if (res.cancel) {
+			  return
+		    }
+		  }
 		})
+	},
+	pay(){
+		wx.showModal({
+		  title: '支付',
+		  content: '是否继续支付',
+		  success (res) {
+		    if (res.confirm) {
+				wx.showToast({
+				  title: '支付成功',
+				  icon: 'success',
+				  duration: 2000
+				})
+		    } else if (res.cancel) {
+		     return
+		    }
+		  }
+		})
+		
 	},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
