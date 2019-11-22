b7794f0d141f8c39bed62b4b8956072f692b71de
guochaonan-523 <51936191+guochaonan-523@users.noreply.github.com>
2019-11-22 11:31:51 +0800

    input框重置按钮

--- wxworksapce/components/MyInput/MyInput.js
+++ wxworksapce/components/MyInput/MyInput.js
@@ -11,13 +11,22 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    inputValue: ''
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    bindKeyInput(e) {
+        this.setData({
+          inputValue: e.detail.value
+        })
+      },
+    resetClick() {
+      this.setData({
+        inputValue: ''
+      })
+    },
   }
 })
